b43679d4740faba0d9adfb599aa6e80b29cbe942
Fox <paul.liverman.iii@gmail.com>
2016-02-03 09:27:43 -0800

    robust-ified-ish

--- src/main.lua
+++ src/main.lua
@@ -1,17 +1,31 @@
+local lg = love.graphics
+local insert = table.insert
+
+local scale = {1, 1}       -- x/y axis scaling
+local domain = {-100, 100} -- domain of graph to draw
+local detail = 1           -- interval between x values
+local offset = {0, 0}      -- x/y offset where are we viewing
+local points = {}          -- points to draw
+
 local fn = function(x)
     return 1/3*(x*x*x) + 3*x*x - 5
 end
 
-local points = {}
+local function getPoints()
+    points = {}
+    for x = domain[1], domain[2], detail do
+        insert(points, x * scale[1])
+        insert(points, -fn(x) * scale[2])
+    end
+end
 
-for i=-250,250 do
-    table.insert(points, i)
-    table.insert(points, fn(i))
+function love.load()
+    getPoints(function(x) return 1/3*x*x*x + 3*x*x -5 end)
 end
 
 function love.draw()
-    love.graphics.translate(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
-    love.graphics.line(points)
+    lg.translate(lg.getWidth()/2 + offset[1], lg.getHeight()/2 + offset[2])
+    lg.line(points)
 end
 
 function love.keypressed(key)
